a701eda5def5e99d374923e1c4743975c64fe926
vallemagnusson <valle.magnusson@gmail.com>
2015-10-16 19:37:16 +0200

    bläää

--- convertMSHtoXML.py
+++ convertMSHtoXML.py
@@ -3,4 +3,10 @@ import os
 mshDir = os.listdir("msh")
 for filename in mshDir:
 	filenameNoExtension = os.path.splitext(filename)[0]
-	os.system("dolfin-convert msh/" + filename "msh/" + filenameNoExtension + ".xml")
+	oldFile = "msh/"+str(filename)
+	newFile = "msh/" + filenameNoExtension + ".xml"
+	os.system("dolfin-convert " + oldFile + " " + newFile)
+
+
+
+#dolfin-convert cylinder6.msh out.xml
