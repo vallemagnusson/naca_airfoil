a12334b353339b154d49192fc14fc8cc0011dee5
vallemagnusson <valle.magnusson@gmail.com>
2015-10-16 19:01:41 +0200

    fuuuuck

--- run.sh
+++ run.sh
@@ -17,7 +17,7 @@
 #
 # EDIT FOLLOWING VARIABLES
 # Path to GMSH binary
-GMSHBIN="/User/bin/gmsh"
+GMSHBIN="/user/bin/gmsh"
 #GMSHBIN="/Applications/Gmsh.app/Contents/MacOS/gmsh"
 # Path to dir where geo files will be stored
 GEODIR="geo"
