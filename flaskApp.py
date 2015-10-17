27fedc4eacbed1758e8ce958da16790fde964415
vallemagnusson <valle.magnusson@gmail.com>
2015-10-18 00:48:03 +0200

    nudå

--- flaskApp.py
+++ flaskApp.py
@@ -34,14 +34,20 @@ def hello_world():
 	#return app.root_path
 	return render_template('index.html', author=author, name=name)
 
+@app.route('/')
 def form():
     return render_template('form_submit.html')
-    
+
 @app.route('/hello/', methods=['POST'])
 def hello():
 	name=request.form['yourname']
 	email=request.form['youremail']
 	return render_template('form_action.html', name=name, email=email)
 
-if __name__ == "__main__":
-	app.run(host="0.0.0.0", debug=True )
+#if __name__ == "__main__":
+#	app.run(host="0.0.0.0", debug=True )
+if __name__ == '__main__':
+  app.run( 
+        host="0.0.0.0",
+        port=int("80")
+  )
