6ae3e2dbf2bba1079c4c5580e5bb497a02ef3dd2
vallemagnusson <valle.magnusson@gmail.com>
2015-10-17 16:19:28 +0200

    massa listor

--- readFile.py
+++ readFile.py
@@ -6,7 +6,7 @@ def readFile(fileName):
 	theFile = open(fileName, "r").read()
 	#print theFile
 	timeColumn = []
-	lifeColumn = []
+	liftColumn = []
 	dragColumn = []
 	#print theFile.split()
 	lines = open(fileName, "r").readlines()
@@ -14,7 +14,14 @@ def readFile(fileName):
 		#content = f.readlines()
 		#print content
 	for x in range(1, len(lines)):
-		print lines[x]
+		time = lines[x].strip().split()[0]
+		timeColumn.append(time)
+		lift = lines[x].strip().split()[1]
+		liftColumn.append(lift)
+		drag = lines[x].strip().split()[2]
+		timeColumn.append(drag)
+		print timeColumn
+		#print lines[x].strip()
 	#for line in theFile.split():
 	#	timeColumn.append(line[0])
 	#print timeColumn
