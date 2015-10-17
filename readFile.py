c05e0d39e3069418c1b7723135dcd6669fafa22b
vallemagnusson <valle.magnusson@gmail.com>
2015-10-17 16:01:50 +0200

    nu då

--- readFile.py
+++ readFile.py
@@ -4,13 +4,18 @@ import sys
 
 def readFile(fileName):
 	theFile = open(fileName, "r").read()
-	print theFile
+	#print theFile
 	timeColumn = []
+	lifeColumn = []
+	dragColumn = []
 	#print theFile.split()
-	lines = open(fileName, "r").readlines()
-	print lines
-	for line in theFile.split():
-		timeColumn.append(line[0])
+	#lines = open(fileName, "r").readlines()
+	with open(fileName, "r") as f:
+    	content = f.readlines()
+    	print content
+	#print lines
+	#for line in theFile.split():
+	#	timeColumn.append(line[0])
 	#print timeColumn
 
 readFile("navier_stokes_solver/results/drag_ligt.m")
