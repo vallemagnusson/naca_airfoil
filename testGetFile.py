a9a5e8241534d6b464a438412d970379daad3281
vallemagnusson <valle.magnusson@gmail.com>
2015-10-18 15:00:19 +0200

    print filename

--- testGetFile.py
+++ testGetFile.py
@@ -9,7 +9,9 @@ app = Flask(__name__)
 def test():
 	appLocation = app.root_path
 	fileLocation = appLocation + "/msh"
+	content = os.listdir(fileLocation)
 	print 1, fileLocation
-	print 2, os.listdir(fileLocation)
+	for i in range(len(content)):
+		print content[i]
 
 test()
