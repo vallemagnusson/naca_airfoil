a316deba1c13b7347fd4e2c4bf33971019137378
vallemagnusson <valle.magnusson@gmail.com>
2015-10-18 15:32:17 +0200

    test from

--- tasks.py
+++ tasks.py
@@ -10,11 +10,11 @@ from celery import Celery
 from collections import Counter
 import urllib2
 
-app = Celery('tasks', backend='amqp', broker='amqp://mava:orkarinte@130.238.29.120:5672/app2')
+app = Celery('proj', backend='amqp', broker='amqp://mava:orkarinte@130.238.29.120:5672/app2')
 
 @app.task
-def convertFile(tweetFileList):
-	
+def convertFile(mshFile):
+		
 	return "dictionary_all"
 
 @app.task
