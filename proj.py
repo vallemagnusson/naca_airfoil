1ff362f8d5021c1583d2195032b183d7ff36de88
vallemagnusson <valle.magnusson@gmail.com>
2015-10-18 16:08:02 +0200

    fan

--- proj.py
+++ proj.py
@@ -19,8 +19,8 @@ def convertFile(fileName, mshFile):
 	if fileName == "r0a0n200.msh":
 		newFile = open(fileName, "w")
 		newFile.write(mshFile)
-		newFile.close()
 		print newFile.read()
+		newFile.close()
 	return "dictionary_all"
 
 @app.task
