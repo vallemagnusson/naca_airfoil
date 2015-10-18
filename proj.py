402cddb172efcc22fd23eb26e8a24167516e0403
vallemagnusson <valle.magnusson@gmail.com>
2015-10-18 15:48:48 +0200

    d

--- proj.py
+++ proj.py
@@ -14,7 +14,7 @@ app = Celery('proj', backend='amqp', broker='amqp://mava:orkarinte@130.238.29.12
 
 @app.task
 def convertFile(fileName, mshFile):
-	if fileName == "r3a9n200.msh":
+	if fileName == "r0a0n200.msh":
 		newFile = open(fileName, "r")
 		newFile = mshFile.write()
 		print newFile
