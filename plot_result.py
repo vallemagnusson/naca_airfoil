d9b3f93914b7d30d5720c2b409f4a36a6b1c85ae
vallemagnusson <valle.magnusson@gmail.com>
2015-10-19 20:42:46 +0200

    a

--- plot_result.py
+++ plot_result.py
@@ -23,9 +23,11 @@ def plot_file(filename, data):
 	#f = open("drag_ligt.m", "r")
 	#s = f.read().split('\n')
 	time = np.array(data[0])
-	print time
+	print 1
 	lift = np.array(data[1])
+	print 2
 	drag = np.array(data[2])
+	print 3
 	#for line in s[1:]:
 	#	l = line.split(' ')
 	#	elemlist = []
@@ -36,16 +38,26 @@ def plot_file(filename, data):
 	#	drag.append(elemlist[1])
 	#	lift.append(elemlist[2])
 	plt.gca().set_color_cycle(['blue', 'red'])
+	print 4
 	plt.plot(time, drag)
+	print 5
 	plt.plot(time, lift)
+	print 6
 	plt.grid(True)
+	print 7
 
 	plt.legend(['Drag force', 'Lift force'])
+	print 8
 	plt.xlabel('Time')
+	print 9
 	plt.ylabel('Force')
+	print 10
 	plt.title(filename)
+	print 11
 	plt.yscale('log')
+	print 12
 	plt.savefig(os.path.splitext(filename)[0] + '.png')
+	print 13
 	#save("signal", ext="png", close=False, verbose=True)
 	#image = open(os.path.splitext(filename)[0] + '.png', "w")
 	#plt.savefig(image, format = "png")
