affe7ec0eaee6131327e994086dad56f5736d6e0
vallemagnusson <valle.magnusson@gmail.com>
2015-10-19 20:33:59 +0200

    np.array()

--- plot_result.py
+++ plot_result.py
@@ -11,6 +11,7 @@ import os
 import matplotlib
 import matplotlib.pyplot as plt
 import os.path
+import numpy as np
 
 ###############################################################################
 # Takes a .m file that is created by the airfoil binary and creates a plot 
@@ -21,9 +22,9 @@ import os.path
 def plot_file(filename, data):
 	#f = open("drag_ligt.m", "r")
 	#s = f.read().split('\n')
-	time = data[0]
-	lift = data[1]
-	drag = data[2]
+	time = np.array(data[0])
+	lift = np.array(data[1])
+	drag = np.array(data[2])
 	#for line in s[1:]:
 	#	l = line.split(' ')
 	#	elemlist = []
@@ -43,6 +44,7 @@ def plot_file(filename, data):
 	plt.ylabel('Force')
 	plt.title(filename)
 	plt.yscale('log')
-	#plt.savefig(os.path.splitext(filename)[0] + '.png')
-	image = open(os.path.splitext(filename)[0] + '.png', "w")
-	plt.savefig(image, format = "png")
+	plt.savefig(os.path.splitext(filename)[0] + '.png')
+	save("signal", ext="png", close=False, verbose=True)
+	#image = open(os.path.splitext(filename)[0] + '.png', "w")
+	#plt.savefig(image, format = "png")
