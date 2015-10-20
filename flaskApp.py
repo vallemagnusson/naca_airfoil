a250c795bc09e5823eb87a0284f2e87fcfdd292a
vallemagnusson <valle.magnusson@gmail.com>
2015-10-20 02:42:00 +0200

    str

--- flaskApp.py
+++ flaskApp.py
@@ -39,7 +39,7 @@ def runsh():
 	for i in range(0, int(n_angles)):
 		angle = 0
 		angle = (int(angle_start) + anglediff * i)
-		angles.append(in_db("r" + n_levels + "a" + angle + "n" + n_nodes + "N" + num_samples + "v" + visc + "s" + speed + "T" + T + ".msh"))
+		angles.append(in_db("r" + n_levels + "a" + str(angle) + "n" + n_nodes + "N" + num_samples + "v" + visc + "s" + speed + "T" + T + ".msh"))
 		
 	response = group(convertFile.s(angle, n_nodes, n_levels, num_samples, visc, speed, T) for angle in angles)
 	result = response.apply_async()
